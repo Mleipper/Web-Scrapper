922cd2b98360218f3b4af2eaf71ac6f7105b7d91
Michael Leipper <michael.leipper@odds-bods.com>
2020-02-13 17:51:56 +0000

    Trying to figure this out

--- WebScrapper/Program.cs
+++ WebScrapper/Program.cs
@@ -7,7 +7,7 @@ namespace WebScrapper
         static async System.Threading.Tasks.Task Main(string[] args)
         {
             var scrapper = new WebScrapper();
-
+            scrapper.GetDateFromAndTo();
             var response = await scrapper.GetRespone();
             scrapper.GetScrapeResults(response);
         }
--- WebScrapper/WebScrapper.cs
+++ WebScrapper/WebScrapper.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace WebScrapper
 {
@@ -16,6 +17,7 @@ namespace WebScrapper
         private readonly string BaseUri = "https://starsports.bet/sport/horseracing";
 
         private readonly string test = "https://www.starsports.bet/api/events/search?market.main=yes&perPage=500&sort=-competition.displayOrder&sort=competition.name&sort=timeSettings.startTime&sport=horseracing&startTime%5Bfrom%5D=2020-02-13T00%3A00%3A00.000Z&startTime%5Bto%5D=2020-02-15T00%3A00%3A00.000Z&tags.star-events=-";
+        //https://www.starsports.bet/api/events/search?market.main=yes&perPage=500&sort=-competition.displayOrder&sort=competition.name&sort=timeSettings.startTime&sport=horseracing&startTime%5Bfrom%5D=13%2f02%2f2020+17%3a37%3a19&startTime%5Bto%5D=2020-02-15T00%3A00%3A00.000Z&tags.star-events=-
 
         public async Task<IHtmlDocument> GetRespone()
         {
@@ -34,6 +36,10 @@ namespace WebScrapper
         public string GetDateFromAndTo()
         {
             var currentDateTime = DateTime.UtcNow;
+
+            //var date = new DateTime('2020-02-13T00T00T00.000Z');
+                       //13%2f02%2f2020+17%3a37%3a19
+            var fromtime = HttpUtility.UrlEncode(currentDateTime.ToString());
 
             return $"from%5D=2020-02-13T00%3A00%3A00.000Z&startTime%5Bto%5D=2020-02-15T00%3A00%3A00.000Z";
         }
